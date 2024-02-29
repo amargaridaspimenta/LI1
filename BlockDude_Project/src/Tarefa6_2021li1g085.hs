1a30e709ae302e68767c649be94b0b77a92fba63
Margarida Sousa Pimenta <94836302+amargaridaspimenta@users.noreply.github.com>
2024-02-29 17:28:43 +0000

    Update Tarefa6_2021li1g085.hs

--- BlockDude_Project/src/Tarefa6_2021li1g085.hs
+++ BlockDude_Project/src/Tarefa6_2021li1g085.hs
@@ -1,9 +1,3 @@
-{- |
-Module      : Tarefa6_2021li1gXXX
-Description : Resolução de um puzzle
-
-Módulo para a realização da Tarefa 6 do projeto de LI1 em 2021/22.
--}
 module Tarefa6_2021li1g085 where
 
 import LI12122
