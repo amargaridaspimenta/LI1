59f47ab827f1134d649bfe86a0b31736981d999a
Margarida Sousa Pimenta <94836302+amargaridaspimenta@users.noreply.github.com>
2024-02-29 17:28:07 +0000

    Update Tarefa3_2021li1g085.hs

--- BlockDude_Project/src/Tarefa3_2021li1g085.hs
+++ BlockDude_Project/src/Tarefa3_2021li1g085.hs
@@ -1,11 +1,3 @@
-{- |
-Module      : Tarefa3_2021li1g085
-Description : Representação textual do jogo
-Copyright   : LingYun Zhu <a100820@alunos.uminho.pt>;
-            : Ana Margarida Sousa Pimenta <a100830@alunos.uminho.pt>;
-
-Módulo para a realização da Tarefa 3 do projeto de LI1 em 2021/22.
--}
 module Tarefa3_2021li1g085 where
 
 import LI12122
